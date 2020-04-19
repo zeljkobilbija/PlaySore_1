abae549469ed0fe03f4cf4a8022eeeba2f92bc17
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-04-19 14:02:50 +0200

    v 1.2 (14) Dodato jos molitvi i ubacen xml file sa tekstom molitvi

--- app/src/main/java/com/interfacemockup/kalendar/HomeActivity.java
+++ app/src/main/java/com/interfacemockup/kalendar/HomeActivity.java
@@ -30,7 +30,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
         GlobalnaClassa glob = GlobalnaClassa.getInstance();
-        glob.setPokaziAdMob(false);
+        glob.setPokaziAdMob(true);
 
 
     }
