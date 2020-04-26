391c19858d5fe114582af3ee659d3460934618a5
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-04-26 12:35:47 +0200

    manje izmene molitve detail

--- app/src/main/java/com/interfacemockup/kalendar/HomeActivity.java
+++ app/src/main/java/com/interfacemockup/kalendar/HomeActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
