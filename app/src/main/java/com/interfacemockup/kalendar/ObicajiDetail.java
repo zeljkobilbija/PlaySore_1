ef6e90ce89d27da91f666c4ca2c6648c6e2bc881
Zeljko Bilbija <bilbijazeljko@gmail.com>
2020-05-03 15:55:54 +0200

    Dodati novi banneri

--- app/src/main/java/com/interfacemockup/kalendar/ObicajiDetail.java
+++ app/src/main/java/com/interfacemockup/kalendar/ObicajiDetail.java
@@ -7,16 +7,27 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class ObicajiDetail extends AppCompatActivity {
 
     private int _selektor;
     private TextView _naslov;
     private  TextView _obicaj;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obicaji_detail);
+
+        MobileAds.initialize(this, "ca-app-pub-7920431183682527~1369121836");
+
+
         _naslov = findViewById(R.id.id_naslov_obicaja);
         _obicaj = findViewById(R.id.id_obicaj_text_detail);
         _obicaj.setMovementMethod(new ScrollingMovementMethod());
@@ -31,5 +42,16 @@ public class ObicajiDetail extends AppCompatActivity {
         _naslov.setText(naslov);
         _obicaj.setText(obicaj);
 
+    }
+
+    private void addMob(){
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.id_obicaji_add);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
